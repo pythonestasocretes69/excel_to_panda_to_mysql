f5cc62f633c9658490c0e1d41d2aed3791826472
pythonestasocretes69 <93080394+pythonestasocretes69@users.noreply.github.com>
2023-05-25 14:20:08 +0530

    Update alterations.py

--- alterations.py
+++ alterations.py
@@ -7,3 +7,10 @@ df = pd.read_csv('stock_report.csv')
 print(df.describe())
 
 
+# this will reindex the panda
+df.index = df['Date']
+df = df.drop('Date',axis=1)
+print(df)
+
+#creation of new csv file will again shift the index
+
