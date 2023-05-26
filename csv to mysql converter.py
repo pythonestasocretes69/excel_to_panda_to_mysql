cafc80b6efcd66b6b7980e724e41fac76477e75e
pythonestasocretes69 <93080394+pythonestasocretes69@users.noreply.github.com>
2023-05-26 16:41:44 +0530

    format change and data extraction done
    The project is changed the source file is now in excel as it is more widely used

--- csv to mysql converter.py
+++ csv to mysql converter.py
@@ -1,16 +1,27 @@
-'''
 import numpy as np
 import pandas as pd
 import mysql.connector as sqlator
 
-def loader(file_name):
-    df = pd.read_csv(file_name)
+
+#panda is loading the file
+file_name = input('Enter file name: ')
+df = pd.read_excel(file_name)
 
 #main
-file_name = input('Enter file name: ')
 table_name = input("Enter the table's name you want to create: ")
 database_name = input ("Enter database name: ")
 
+columns = list(df.columns)                                                           # extraction of primary key & columns
+print(columns)
+position= int(input('Enter the positon of the column to be taken as a primary key:'))
+print(columns[position],'is the primary key')
+primary_key = columns[position]
+
+column_types = []                                                                     #extraction of datatypes of columns
+for i in columns:
+    object = (df.loc[0,i])
+    column_types+= [type(object)]
+'''
 #mysql_connector
 mycon = sqlator.connect(host='localhost', user='root', passwd='Plank#6.626', database=database_name)
 if mycon.is_connected():
