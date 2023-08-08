3e46968b5aba7bed25b371e91bfffaa3316bc55d
pythonestasocretes69 <93080394+pythonestasocretes69@users.noreply.github.com>
2023-08-08 19:16:20 +0530

    Update correlation.py
    Added menue

--- correlation.py
+++ correlation.py
@@ -5,17 +5,21 @@ location = 'Scores.xlsx'
 df = pd.read_excel(location)
 description = df.describe()
 
-MMean = description.loc['mean', 'Mathematics']
-PMean = description.loc['mean', 'Physics']
-CMean = description.loc['mean', 'Chemistry']
-TMean = description.loc['mean', 'Total']
+# menue addidng
+menue =  df.columns()
+primary_i = int(input ('Enter index of primary factor' + str(menue))) 
+secondary_i = int(input ('Enter index of secondary factor' + str(menue))) 
+primary = menue.pop(primary_i)
+secondary = menue.pop(secondary_i)
+PMean = description.loc('avg',primary)
+SMean = description.loc('avg',secondary)
 
 # covariance
 cov = 0
 for i in df.index:
-    term = (TMean - df.loc[i, 'Total']) * (MMean - df.loc[i, 'Mathematics'])
+    term = (PMean - df.loc[i, primary]) * (SMean - df.loc[i, secondary])
     cov += term
 cov /= len(df.index)
 print('cov=', cov)
-corr = cov / (description.loc['std', 'Mathematics'] * description.loc['std', 'Total'])
+corr = cov / (description.loc['std', secondary] * description.loc['std', primary])
 print('corr=', corr)
