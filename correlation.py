87a4eb7b282268f5022631e1c4eed7883e448be2
pythonestasocretes69 <93080394+pythonestasocretes69@users.noreply.github.com>
2023-08-08 19:44:17 +0530

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
 # get the table
-location = 'Scores.xlsx'
+location = input('Enter file name')
 df = pd.read_excel(location)
 description = df.describe()
 
 # menue addidng
-menue =  df.columns()
+menue =  list(df.columns)
 primary_i = int(input ('Enter index of primary factor' + str(menue))) 
 secondary_i = int(input ('Enter index of secondary factor' + str(menue))) 
 primary = menue.pop(primary_i)
 secondary = menue.pop(secondary_i)
-PMean = description.loc('avg',primary)
-SMean = description.loc('avg',secondary)
+PMean = description.loc['mean',primary]
+SMean = description.loc['mean',secondary]
 
 # covariance
 cov = 0
